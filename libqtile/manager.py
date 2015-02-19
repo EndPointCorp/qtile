4cea4b548136bdcb691c5b381da117c5480a2ef5
Matt Vollrath <matt@endpoint.com>
2015-02-19 04:23:26 -0500

    Don't allow onscreen keyboard to steal focus

--- libqtile/manager.py
+++ libqtile/manager.py
@@ -867,7 +867,9 @@ class Qtile(command.CommandObject):
                 [xcffib.xproto.StackMode.Above]
             )
 
-        if self.windowMap.get(wnd):
+        w = self.windowMap.get(wnd)
+
+        if w and not w.get_property("ONSCREEN_KEYBOARD"):
             self.currentGroup.focus(self.windowMap.get(wnd), False)
             self.windowMap.get(wnd).focus(False)
 
